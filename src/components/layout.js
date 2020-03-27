e816f67eb05ad31bd892115c6c0d38b48ca0b6e8
Christopher Josephs <chrisjosephs1024@gmail.com>
2020-03-27 20:17:39 +0000

    Add threejs

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@
  */
 
 import React from "react"
-import StarfieldAnimation from "./starfieldcanvas";
+import BigBangStarField from 'react-big-bang-star-field'
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
@@ -14,14 +14,6 @@ import tw from 'tailwind.macro'
 import styled from '@emotion/styled'
 import "./layout.css"
 import '../css/global.css'
-
-const PageContainer = styled.div`
-    ${tw`
-        bg-transparent text-xl text-center
-    `}
-    padding: 0px;
-`;
-
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -34,41 +26,42 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <div
-    style={{
-        'overflow-x': 'hidden',
-        'padding': '0px; margin: 0px',
-        'width': '100%',
-        'height': '100%'
-    }}>
-        <StarfieldAnimation
-            style={{
-                position: 'absolute',
-                width: '100%',
-                height: '100%'
-            }}
-        />
-    </div>
-  )
+    return (
+        <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div
+                style={{
+                    'overflow-x': 'hidden',
+                    'padding': '0px; margin: 0px',
+                    'width': '100%',
+                    'height': '100%'
+                }}>
+                <BigBangStarField
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    zIndex: '-10'
+                }}
+            />
+                <div
+                    style={{
+                        margin: `0 auto`,
+                        maxWidth: 960,
+                        padding: `0 1.0875rem 1.45rem`,
+                    }}
+                >
+                <main>{children}</main>
+                    </div>
+                <footer>
+                    © {new Date().getFullYear()}, Built with
+                    {` `}
+                    <a href="https://www.gatsbyjs.org">Gatsby</a>
+                </footer>
+            </div>
+        </>
+    )
 }
-var old = `  <Header siteTitle={data.site.siteMetadata.title} />
-        <PageContainer>
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0 1.0875rem 1.45rem',
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-        </PageContainer>`;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
