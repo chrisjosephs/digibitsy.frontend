7d1e28e84861186750f2c1aabfa2e6f4eef6a418
Christopher Josephs <chrisjosephs1024@gmail.com>
2020-03-27 20:23:54 +0000

    make three rendered background transparent

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,8 @@ const IndexPage = () => (
 // === THREE.JS CODE START ===
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ alpha: true } );
+renderer.setClearColor( 0x000000, 0 ); // the default
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
