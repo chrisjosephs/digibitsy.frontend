fdeb5844a2bcc2d89f3542961be1748c051b98a9
Christopher Josephs <chrisjosephs1024@gmail.com>
2020-04-15 11:27:09 +0100

    single persistant layout and transitions

---
+++ src/layouts/index.js
@@ -0,0 +1,74 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import BigBangStarField from 'react-big-bang-star-field'
+import PropTypes from "prop-types"
+import {useStaticQuery, graphql} from "gatsby"
+import Header from "../components/header"
+import tw from 'tailwind.macro'
+import styled from '@emotion/styled'
+import "./layout.css"
+import '../css/global.css'
+import {StaticQuery} from "../../.cache/gatsby-browser-entry";
+import Transition from '../components/transition'
+
+const Layout = ({ children, location }) => (
+    <StaticQuery
+        query={graphql`query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }`}
+        render={data => (
+            <>
+                <Header siteTitle={data.site.siteMetadata.title}/>
+                <div
+                    style={{
+                        'overflowX': 'hidden',
+                        'padding': '0px; margin: 0px',
+                        'width': '100%',
+                        'height': '100%'
+                    }}>
+                    <BigBangStarField
+                        className="Big-Bang-Star-Field"
+                        style={{
+                            position: 'absolute',
+                            width: '100%',
+                            height: '100%',
+                            zIndex: '-10'
+                        }}
+                    />
+                    <div
+                        style={{
+                            margin: `0 auto`,
+                            maxWidth: 960,
+                            padding: `0 1.0875rem 1.45rem`,
+                        }}
+                    >
+                    <Transition location = {location}>
+                            <main>{children}</main>
+                    </Transition>
+                    </div>
+                    <footer>
+                        © {new Date().getFullYear()}, Built with
+                        {` `}
+                        <a href="https://www.gatsbyjs.org">Gatsby</a>, Threejs (where opengl supported), Drupal 8,
+                        graphQL, Tailwind css
+                    </footer>
+                </div>
+            </>
+        )}
+    />
+)
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React, {Suspense, useEffect, useMemo, useRef, useState} from 'react'
 import {Link} from "gatsby"
 
-import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import {Canvas, useLoader, useFrame, useThree} from "react-three-fiber"
@@ -82,9 +81,8 @@ function Box(props) {
         </mesh>
     )
 }
-
 const IndexPage = () => (
-    <Layout>
+    <div className={"IndexPage"}>
         <SEO title="Home"/>
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
@@ -101,8 +99,7 @@ const IndexPage = () => (
                 <Model/>
             </Suspense>
         </Canvas>
-    </Layout>
+    </div>
 )
-
 
 export default IndexPage
