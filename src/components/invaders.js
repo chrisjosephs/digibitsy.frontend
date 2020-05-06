e3844fbb436894ee218729568dc5b314b5252c40
Christopher Josephs <chrisjosephs1024@gmail.com>
2020-05-06 17:02:49 +0100

    add space invaders base code

--- src/components/invaders.js
+++ src/components/invaders.js
@@ -1,29 +1,58 @@
-import styled from 'styled-components';
+import styled, {keyframes, css} from 'styled-components';
 import React, {Component} from "react";
+import tw from 'tailwind.macro'
 
 class Invaders extends Component {
     constructor(props) {
         super(props)
-        this.state = {curtains: false}
+        this.state = {
+            curtains: false
+        }
+    }
+
+    toggleCurtains() {
+        this.setState(curtains => {
+            return {curtains: !this.state.curtains};
+        });
     }
 
     render() {
-        const fade = this.state.fade
         return (
-            <div className={"SpaceInvader"} style={{position: "absolute", width:"100%", height: "100%"}}>
-                <FullScreenInvaders className={fade ? FullscreenShow: ''}>>
-                <ButtonSi onClick={() => this.setState({ fade: true })}/>
-                <SpaceInvaderCurtain/>
-                </FullScreenInvaders>
+            <div className={"SpaceInvader"} style={{position: "absolute", width: "100%", height: "100%"}}>
+                <ButtonSi onClick={() => this.toggleCurtains()}/>
+                <FullScreenInvaders curtains={this.state.curtains}
+                                    className={this.state.curtains ? FullscreenShow : ''}/>
             </div>
         );
     }
 }
-const Heading = keyframes`
-  0% { top: -3.125em; }
-  100% { top: 3em;}
+
+/**
+ * Bring the Space Invaders curtain down in 8 steps
+ * @type {Keyframes}
+ */
+const curtain = keyframes`
+  0% { margin-top: -3.125em; }
+  6.25%, 12.5% { margin-top: 0em; }
+  18.75%, 25% { margin-top: 3em; }
+  31.25%, 37.5% { margin-top: 6em; }
+  43.75%, 50% { margin-top: 9em; }
+  56.25%, 62.5% { margin-top: 12em; }
+  69.75%, 75% { margin-top: 15em; }
+  81.25%, 87.5% { margin-top: 18em; }
+  93.75%, 100% { margin-top: 21em; }
 `;
-const SpaceInvaderCurtain = styled.div`
+
+const nocurtain = keyframes`
+  0% { margin-top:0; }
+  100% { margin-top: 0;}
+`;
+const FullScreenInvaders = styled.div`
+${tw`w-full inline-block bg-pink-500 bottom-0 right-0 left-0 h-screen w-full`}
+animation: ${props => props.curtains ? css`${curtain}` : css`${nocurtain}`}  8s linear infinite
+`
+
+const SpaceInvaderCurtainOld = styled.div`
   display: inline-block;
   background: pink;
   width: 200px;
@@ -32,15 +61,6 @@ const SpaceInvaderCurtain = styled.div`
   &:hover {
     transform: translate(200px, 150px) rotate(20deg)
   }
-`
-const FullScreenInvaders = styled.div`
-    opacity: 1;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    background: blue; 
-    height: 100%;
-    width: 100%;
 `
 
 const ButtonSi = styled.div`
